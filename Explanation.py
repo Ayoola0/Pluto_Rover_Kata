a34be232903f196c3fecbbc7d9ee457221e4e345
Ayoola0 <34640689+Ayoola0@users.noreply.github.com>
2017-12-18 19:39:09 +0000

    Create Explanation.py

---
+++ Explanation.py
@@ -0,0 +1,15 @@
+I first want to define all possible movement, with consideration for different starting positions and directions
+I kept start position as user input
+I gave the user ability to change the position and direction( only one at a time) 
+I became aware of the need to creat a map/grid
+I went on to learn how to create a grid (see grid.py)
+I tried to learn to how traverse the grid to no avail
+I understand theoretically how wrapping works but not how to implement it in code form eg
+On a 4 x 4 grid, if the rover is sitting at (0,4,N), an 'F' command should place it on (0,0,N)
+Upon further research, I learned I will need to use 'class'
+This is something I have not yet come across, nor was I able to understand it
+As I want this to be a true reflection of my current ability, I opted out of using it
+I have added Object Oriented Progamming to my personal development timetable to help me better understand to to approach problems like this
+Upon learning about Test Driven Developmemt, I quickly became aware of a higher proficiency of python that I am eager but yet to attain.
+I have now decided to sign up for Python classes offline so i can learn with some structure
+
